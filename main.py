eb1768bc2d494f0537460069ec65094d3b2867b6
Austin Rivera <austin.rivera.24@aol.com>
2024-05-23 17:00:42 -0400

    setup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+
 run = True
 menu = True
 play = False
@@ -6,12 +9,41 @@ rules = False
 # (# = choice input)
 #  (> = empty input)
 
+HP = 50
+ATK = 3
+
+
+def clear():
+    if os.name == 'nt':  # For Windows
+        os.system('cls')
+    else:  # For Unix-like systems (Linux, macOS, etc.)
+        os.system('clear')
+
+def draw():
+    print("=========================")
+
+def save():
+    list = [
+        name,
+        str(HP),
+        str(ATK)
+    ]
+
+    f = open("load.txt", "w")
+
+    for item in list:
+        f.write(item + "\n")
+    f.close()
+
 while run:
     while menu:
+        clear()
+        draw()
         print('1. NEW GAME')
         print('2. LOAD GAME')
         print('3. RULES')
         print('4. QUIT GAME')
+        draw()
 
         if rules:
             print("Here are some rules.")
@@ -23,21 +55,37 @@ while run:
 
 
         if choice =="1":
+            clear()
             name = input("# What's your name, hero? ")
             menu = False
             play = True
         elif choice =="2":
-            pass
+            f = open("load.txt", "r")
+            load_list = f.readlines()
+            name = load_list[0][:-1]
+            HP = load_list[1][:-1]
+            ATK = load_list[2][:-1]
+            clear()
+            print("Welcome back, " + name + "!")
+            input("> ")
+            menu = False
+            play = True
         elif choice == "3":
             rules = True
         elif choice == "4":
             quit()
 
     while play: 
-        print(name)
+        save() #autosave
+
+        clear()
+        draw()
+        print("0 = SAVE & QUIT")
+        draw()
 
         dest = input("# ")
 
-        if dest - "0":
+        if dest == "0":
             play = False
             menu = True
+            save()
